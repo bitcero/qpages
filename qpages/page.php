0f9678f3b5382a46ed22f845349612b39c6a7c77
mambax7 <mambax7@gmail.com>
2014-11-18 08:38:32 -0500

    cat should be category

--- qpages/page.php
+++ qpages/page.php
@@ -8,10 +8,10 @@
 // License: GPL 2.0
 // --------------------------------------------------------------
 
-if (!defined("XOOPS_MAINFILE_INCLUDED")){
+if (!defined("XOOPS_MAINFILE_INCLUDED")) {
 	require '../../mainfile.php';
 	$header = array();
-	foreach ($_REQUEST as $k => $v){
+	foreach ($_REQUEST as $k => $v) {
 		$header[$k] = $v;
 	}
 }
@@ -19,13 +19,13 @@ if (!defined("XOOPS_MAINFILE_INCLUDED")){
 function qp_assign_page( $page ){
     global $xoopsTpl;
     $xoopsTpl->assign('page', array(
-        'title'		=> $page->title,
-        'text'		=> $page->content,
-        'id'		=> $page->id(),
-        'name'		=> $page->nameid,
-        'mod_date'	=> sprintf(__('Last update: %s', 'qpages'), formatTimestamp($page->modified,'c')),
-        'reads'		=> sprintf(__('Read %u times','qpages'), $page->hits),
-        'metas'		=> $page->get_meta()
+        'title'        => $page->title,
+        'text'        => $page->content,
+        'id'        => $page->id(),
+        'name'        => $page->nameid,
+        'mod_date'    => sprintf(__('Last update: %s', 'qpages'), formatTimestamp($page->modified,'c')),
+        'reads'        => sprintf(__('Read %u times','qpages'), $page->hits),
+        'metas'        => $page->get_meta()
     ));
 }
 
@@ -33,7 +33,7 @@ load_mod_locale('qpages');
 
 $xoopsOption['module_subpage'] = 'page';
 
-if (isset($_REQUEST['page'])){
+if (isset($_REQUEST['page'])) {
     $nombre = explode('/',$_REQUEST['page']);
 } else {
     $nombre = explode('/',$request);
@@ -42,24 +42,24 @@ if (isset($_REQUEST['page'])){
 $nombre[0] = TextCleaner::sweetstring($nombre[0]);
 
 $page = new QPPage($nombre[0]);
-if ($page->isNew() || $page->getVar('public')==0){
+if ($page->isNew() || $page->getVar('public')==0) {
 	QPFunctions::error_404();
 	die();
 }
 
-if (!in_array(0, $page->groups )){
-	if (empty($xoopsUser)){
+if (!in_array(0, $page->groups )) {
+	if (empty($xoopsUser)) {
 		redirect_header(QP_URL, 2, _MS_QP_NOALLOWED);
 		die();
 	} else {
 		$ok = false;
-		foreach ($xoopsUser->getGroups() as $k){
+		foreach ($xoopsUser->getGroups() as $k) {
 			if ($ok) continue;
-			if (in_array($k, $page->groups)){
+			if (in_array($k, $page->groups)) {
 				$ok = true;
 			}
 		}
-		if (!$ok && !$xoopsUser->isAdmin()){
+		if (!$ok && !$xoopsUser->isAdmin()) {
 			redirect_header(QP_URL, 2, _MS_QP_NOALLOWED);
 			die();
 		}
@@ -68,14 +68,14 @@ if (!in_array(0, $page->groups )){
 
 $page->addHit();
 
-if ($page->type == 'redir'){
+if ($page->type == 'redir') {
 	header('location: '.$page->url);
 	die();
 }
 
 $catego = new QPCategory($page->category);
 
-if ( $page->template != '' ){
+if ($page->template != '') {
 
     $file = XOOPS_ROOT_PATH . $page->template;
     $file_data = pathinfo( $page->template );
@@ -100,7 +100,7 @@ if ( $page->template != '' ){
 
     $xoopsTpl->assign('xoops_pagetitle', $page->title);
 
-    if ( substr($page->template, -4 ) == '.php'){
+    if ( substr($page->template, -4 ) == '.php') {
         if ( isset( $template->Standalone ) && $template->Standalone )
 
             include $file;
@@ -134,11 +134,11 @@ $idp = 0; # ID de la categoria padre
 $rutas = array();
 $path = explode('/',$catego->getPath());
 $tbl = $db->prefix("mod_qpages_categos");
-foreach ($path as $k){
+foreach ($path as $k) {
 	if ($k=='') continue;
 	$sql = "SELECT id_cat FROM $tbl WHERE nameid='$k' AND parent='$idp'";
 	$result = $db->query($sql);
-	if ($db->getRowsNum($result)>0){
+	if ($db->getRowsNum($result)>0) {
 		list($idp) = $db->fetchRow($result);
 		$rutas[] = new QPCategory($idp);
 	}
@@ -147,14 +147,14 @@ foreach ($path as $k){
 $location = '<a href="'.QP_URL.'" title="'.$xoopsModule->name().'">'.$xoopsModule->name().'</a> ';
 $pt = array(); // Titulo de la página
 $pt[] = $xoopsModule->name();
-foreach($rutas as $k){
+foreach ($rutas as $k) {
 	$location .= '&raquo; <a href="'.$k->permalink().'">'.$k->name.'</a> ';
 	$pt[] = $k->name;
 }
 $location .= '&raquo; '.$page->title;
 $pt[] = $page->title;
 $pagetitle = '';
-for ($i=count($pt)-1;$i>=0;$i--){
+for ($i=count($pt)-1;$i>=0;$i--) {
 	$pagetitle .= $pagetitle=='' ? $pt[$i] : " &laquo; $pt[$i]";
 }
 
@@ -174,11 +174,11 @@ $rmf->add_keywords_description($description!='' ? $description : '', $keywords!=
 RMTemplate::get()->add_meta( 'title', $page->getVar( 'custom_title' ) );
 
 // Páginas relacionadas
-if ($mc['related']){
-	$sql = "SELECT * FROM ".$db->prefix("mod_qpages_pages")." WHERE cat='".$catego->id()."' AND id_page<>'".$page->id()."' ORDER BY RAND() DESC LIMIT 0,$mc[related_num]";
+if ($mc['related']) {
+	$sql = "SELECT * FROM ".$db->prefix("mod_qpages_pages")." WHERE category='".$catego->id()."' AND id_page<>'".$page->id()."' ORDER BY RAND() DESC LIMIT 0,$mc[related_num]";
 	$result = $db->query($sql);
 	$tpl->assign('related_num', $db->getRowsNum($result));
-	while ($row = $db->fetchArray($result)){
+	while ($row = $db->fetchArray($result)) {
 		$rp = new QPPage();
 		$rp->assignVars($row);
 		$tpl->append('related', array('id'=>$rp->id(),'link'=>$rp->permalink(),'title'=>$rp->title,
